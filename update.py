52b210fb9a411cf7628b20f6e713be5d6bb4c240
Meow404 <73886809+penucuriCode@users.noreply.github.com>
2021-10-27 17:23:24 +0700

    Create update.py

---
+++ update.py
@@ -0,0 +1,5 @@
+import os
+print("\n\033[32;1mUpdating Wordlist...\n\n\033[0m")
+os.system('mv wordlist.txt wordlist_lama.txt')
+os.system('wget https://raw.githubusercontent.com/penucuriCode/kcfinder/main/wordlist.txt --no-check-certificate')
+print("\033[32;1mUpdate Wordlist Success\n\033[0m")
